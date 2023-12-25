bf00faac94f05dc75dfd3d06e00c0fee84c584af
joolean14 <Joolean14>
2023-12-25 12:59:56 -0500

    first commit

---
+++ resources/views/edit-post.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Edit post</h1>
+    <form action="/edit-post/{{$post->id}}" method="POST"></form>
+        @csrf
+        @method('PUT')
+        <input type="text" name="title" value="{{$post->title}}">
+        <textarea name="body">{{$post->body}}</textarea>
+        <button>save Changes</button>
+</body>
+</html>
